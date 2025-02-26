edf2f03c30ad8a07ac8b991a0614da9f4a8c7fd4
Nicole Go <nicoleam.go@gmail.com>
2025-02-26 07:52:57 -0500

    Initial Commit

---
+++ Transactions-Search-Page/Transactions_Search_PageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Transactions_Search_PageApp.swift
+//  Transactions-Search-Page
+//
+//  Created by Nicole Go on 2025-02-26.
+//
+
+import SwiftUI
+
+@main
+struct Transactions_Search_PageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
